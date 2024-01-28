5ebbd22f0d8879bd7568bedf1fba0686677ab0d3
mattgen <magenoud@gmail.com>
2024-01-28 21:22:37 +0100

    Added TripleOscillator.cs (Allows to modulate sounds, see IEnumerator PLAYSOUND()) SAYMONSI (SimonSays ->> purple button) almost complete

--- Assets/DTASCRIPT.cs
+++ Assets/DTASCRIPT.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DTASCRIPT : MonoBehaviour
 {
@@ -14,6 +15,15 @@ public class DTASCRIPT : MonoBehaviour
     public GameObject BLUEbutton;
     public GameObject PURPLEbutton;
 
+    public Button RED;
+    public Button YELLOW;
+    public Button GREEN;
+    public Button BLUE;
+    public Button PURPLE;
+
+    public Image SAYMONSI;
+    TripleOscillator TO;
+
     UnityEngine.UI.Text text;
 
 
@@ -24,6 +34,13 @@ public class DTASCRIPT : MonoBehaviour
         _infos = Infos.instance;
 
         text = FindObjectOfType<UnityEngine.UI.Text>();
+        TO = FindObjectOfType<TripleOscillator>();
+
+        RED.onClick.AddListener(DONTCLICKRED);
+        YELLOW.onClick.AddListener(DONTCLICKYELLOW);
+        GREEN.onClick.AddListener(DONTCLICKGREEN);
+        BLUE.onClick.AddListener(DONTCLICKBLUE);
+        PURPLE.onClick.AddListener(DONTCLICKPURPLE);
     }
 
 
@@ -55,7 +72,7 @@ public class DTASCRIPT : MonoBehaviour
     void Update()
     {
         elapsedTime += Time.deltaTime;
-        if(elapsedTime >= 20)
+        if(elapsedTime >= 30)
         {
             if(_hasClicked == true)
             {
@@ -74,41 +91,223 @@ public class DTASCRIPT : MonoBehaviour
 
     public void DONTCLICKRED()
     {
-        Debug.Log("Clicked RED");
-        elapsedTime = 17;
-        _hasClicked = true;
-        REDbutton.transform.Translate(0, 0, -0.2f,Space.Self); 
+        if (SAYMONSISTARTED != true)
+        {
+            if (_hasClicked != true)
+            {
+                Debug.Log("Clicked RED");
+                elapsedTime = 17;
+                _hasClicked = true;
+                StartCoroutine(ButtonPressed(REDbutton,0));
+                //UnityEngine.UI.Button[] buttons = FindObjectsOfType<UnityEngine.UI.Button>();
+                //foreach(UnityEngine.UI.Button b in buttons)
+                //{
+                //    b.interactable = false;
+                //}
+            }
+        }
+        else
+        {
+            currentValue = 0;
+            StartCoroutine(ButtonPressed(REDbutton,0));
+        }
     }
 
     public void DONTCLICKYELLOW()
     {
-        Debug.Log("Clicked YELLOW");
-        elapsedTime = 17;
-        _hasClicked = true;
-        YELLOWbutton.transform.Translate(0, 0, -0.2f, Space.Self);
+        if (SAYMONSISTARTED != true)
+        {
+            if (_hasClicked != true)
+            {
+                Debug.Log("Clicked YELLOW");
+                elapsedTime = 17;
+                _hasClicked = true;
+                StartCoroutine(ButtonPressed(YELLOWbutton,1));
+            }
+        }
+        else
+        {
+            currentValue = 1;
+            StartCoroutine(ButtonPressed(YELLOWbutton,1));
+        }
+
     }
 
     public void DONTCLICKGREEN()
     {
-        Debug.Log("Clicked GREEN");
-        elapsedTime = 17;
-        _hasClicked = true;
-        GREENbutton.transform.Translate(0, 0, -0.2f, Space.Self);
+        if (SAYMONSISTARTED != true)
+        {
+            if (_hasClicked != true)
+            {
+                Debug.Log("Clicked GREEN");
+                elapsedTime = 17;
+                _hasClicked = true;
+                StartCoroutine(ButtonPressed(GREENbutton,2));
+
+            }
+        }
+        else
+        {
+            currentValue = 2;
+            StartCoroutine(ButtonPressed(GREENbutton,2));
+        }
+    }
+
+    public void DONTCLICKBLUE()
+
+    {
+        if (SAYMONSISTARTED != true)
+        {
+            if (_hasClicked != true)
+            {
+
+                elapsedTime = 17;
+                _hasClicked = true;
+                StartCoroutine(ButtonPressed(BLUEbutton,3));
+            }
+        }
+        else
+        {
+            currentValue = 3;
+            StartCoroutine(ButtonPressed(BLUEbutton,3));
+        }
     }
 
     public void DONTCLICKPURPLE()
     {
-        Debug.Log("Clicked PURPLE");
-        elapsedTime = 17;
-        _hasClicked = true;
-        PURPLEbutton.transform.Translate(0, 0, -0.2f, Space.Self);
+        if (SAYMONSISTARTED != true)
+        {
+            if (_hasClicked != true)
+            {
+                _hasClicked = true;
+                SAYMONSISTARTED = true;
+
+                elapsedTime = -300;
+                _hasClicked = true;
+                StartCoroutine(ButtonPressed(PURPLEbutton,4));
+                List<int> indices = new List<int>();
+                indices.Add((int)Random.Range(0, 5));
+                SAYMONSIGAME(indices);
+            }
+        }
+        else
+        {
+            currentValue = 4;
+            StartCoroutine(ButtonPressed(PURPLEbutton,4));
+        }
     }
-    public void DONTCLICKBLUE()
-       
+  
+    int currentValue;
+    bool SAYMONSISTARTED = false;
+    bool LOSTSAYMONSI = false;
+    List<int> SAYMONSIINDICES = new List<int>();
+    float SAYMONSISPEED = 1.6f;
+    void SAYMONSIGAME(List<int> indices)
     {
-        Debug.Log("Clicked BLUE");
-        elapsedTime = 17;
-        _hasClicked = true;
-        BLUEbutton.transform.Translate(0, 0, -0.2f, Space.Self);
+     
+        
+
+        StartCoroutine(SimonSequence(SAYMONSISPEED));
+        //for(int i = 0;i<indices.Count;i++)
+        //{
+        //    StartCoroutine(LIGHTBUTTON((ColorSAYMONSI)indices[i], SAYMONSISPEED));
+        //}
+        
+        SAYMONSISPEED -= 0.05f;
+    }
+    IEnumerator ButtonPressed(GameObject button, int buttonIndex)
+    {
+        StartCoroutine(TO.PLAYSOUND(buttonIndex, 0.2f));
+        button.transform.Translate(0, 0, -0.2f, Space.Self);
+        yield return new WaitForSeconds(0.2f);
+        button.transform.Translate(0, 0, 0.2f, Space.Self);
+
+
+    }
+
+    IEnumerator SimonSequence(float duration)
+    {
+        while (true)
+        {
+            // Generate a random color index and add it to the sequence
+            text.text = "I SAY :";
+            int randomIndex = Random.Range(0, System.Enum.GetValues(typeof(ColorSAYMONSI)).Length);
+            SAYMONSIINDICES.Add(randomIndex);
+
+            // Show the sequence to the player
+            foreach (int index in SAYMONSIINDICES)
+            {
+                StartCoroutine(LIGHTBUTTON((ColorSAYMONSI)index,SAYMONSISPEED));
+                yield return new WaitForSeconds(duration);
+            }
+            text.text = "YOU SAY :";
+            // Wait for player input
+            yield return StartCoroutine(PlayerInput());
+        }
+    }
+
+    IEnumerator PlayerInput()
+    {
+        int currentIndex = 0;
+        while (currentIndex < SAYMONSIINDICES.Count)
+        {
+            // Check if player input matches the current color in the sequence
+            if (currentValue == 0 && SAYMONSIINDICES[currentIndex] == (int)ColorSAYMONSI.RED ||
+                currentValue == 1 && SAYMONSIINDICES[currentIndex] == (int)ColorSAYMONSI.YELLOW ||
+                currentValue == 2 && SAYMONSIINDICES[currentIndex] == (int)ColorSAYMONSI.GREEN ||
+                currentValue == 3 && SAYMONSIINDICES[currentIndex] == (int)ColorSAYMONSI.BLUE ||
+                currentValue == 4 && SAYMONSIINDICES[currentIndex] == (int)ColorSAYMONSI.PURPLE)
+            {
+                text.text = "Correct!";
+                currentIndex++;
+            }
+            else
+            {
+                text.text = "Incorrect!";
+                // Handle incorrect input (e.g., game over logic)
+                yield break;
+            }
+            yield return null;
+        }
+    }
+
+    IEnumerator LIGHTBUTTON(ColorSAYMONSI color, float duration)
+    {
+        Color initColor = Color.clear;
+        Color actualColor = new Color();
+        switch(color)
+        {
+            case ColorSAYMONSI.RED:
+                actualColor = Color.red;
+                break;
+            case ColorSAYMONSI.YELLOW:
+                actualColor = Color.yellow;
+                break;
+            case ColorSAYMONSI.GREEN:
+                actualColor = Color.green;
+                break;
+            case ColorSAYMONSI.BLUE:
+                actualColor = Color.blue;
+                break;
+            case ColorSAYMONSI.PURPLE:
+                actualColor = Color.magenta;
+                break;
+        }
+        SAYMONSI.color = actualColor;
+        StartCoroutine(TO.PLAYSOUND((int)color, duration));
+        yield return new WaitForSeconds(duration);
+        SAYMONSI.color = initColor;
+    }
+
+    public enum ColorSAYMONSI
+    {
+        RED = 0,
+        YELLOW = 1,
+        GREEN = 2,
+        BLUE = 3,
+        PURPLE = 4
+
     }
 }
+
+
---
+++ Assets/Scripts/TripleOscillator.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TripleOscillator : MonoBehaviour
+{
+    [Range(1, 20000)]  //Creates a slider in the inspector
+    public float frequency1;
+
+    [Range(1, 20000)]  //Creates a slider in the inspector
+    public float frequency2;
+
+    [Range(1, 20000)]  //Creates a slider in the inspector
+    public float frequency3;
+
+    public float sampleRate = 44100;
+    public float waveLengthInSeconds = 2.0f;
+
+    AudioSource audioSource;
+    int timeIndex = 0;
+
+    void Start()
+    {
+    
+        audioSource = gameObject.GetComponent<AudioSource>();
+        audioSource.playOnAwake = false;
+        audioSource.spatialBlend = 0; //force 2D sound
+        audioSource.Stop(); //avoids audiosource from starting to play automatically
+    }
+
+    
+
+    public IEnumerator PLAYSOUND(int color, float duration)
+    {
+        switch(color)
+        {
+            case 0:  //RED
+                frequency1 = 200;
+                frequency2 = 450;
+                frequency3 = 1000;
+                break;
+            case 1:  //YELLOW
+                frequency1 = 350;
+                frequency2 = 800;
+                frequency3 = 1300;
+                break;
+            case 2: //GREEN
+                frequency1 = 500;
+                frequency2 = 1000;
+                frequency3 = 1600;
+                break;
+            case 3: //BLUE
+                frequency1 = 180;
+                frequency2 = 360;
+                frequency3 = 600;
+                break;
+            case 4: //PURPLE
+                frequency1 = 120;
+                frequency2 = 360;
+                frequency3 = 980;
+                break;
+        }
+        timeIndex = 0;
+        audioSource.Play();
+        yield return new WaitForSeconds(0.2f);
+        audioSource.Stop();
+            
+    }
+
+    void OnAudioFilterRead(float[] data, int channels)
+    {
+        for (int i = 0; i < data.Length; i += channels)
+        {
+            data[i] = CreateSine(timeIndex, frequency1, sampleRate);
+
+            if (channels == 2)
+                data[i + 1] = CreateSine(timeIndex, (frequency1* frequency2)/frequency3, sampleRate);
+
+            timeIndex++;
+
+            //if timeIndex gets too big, reset it to 0
+            if (timeIndex >= (sampleRate * waveLengthInSeconds))
+            {
+                timeIndex = 0;
+            }
+        }
+    }
+
+    //Creates a sinewave
+    public float CreateSine(int timeIndex, float frequency, float sampleRate)
+    {
+        return Mathf.Sin(2 * Mathf.PI * timeIndex * frequency / sampleRate);
+    }
+}
